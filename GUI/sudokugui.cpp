2c3f69a6c05fd622c4a738722fb8c9115aebb736
Wolfia <artea@o2.pl>
2014-11-06 22:56:47 +0100

    :<

--- GUI/sudokugui.cpp
+++ GUI/sudokugui.cpp
@@ -1,12 +1,24 @@
 #include "sudokugui.h"
 
-SudokuGUI::SudokuGUI(QWidget *parent)
-	: QMainWindow(parent)
-{
-	ui.setupUi(this);
-}
 
-SudokuGUI::~SudokuGUI()
-{
 
-}
+		SudokuGUI::SudokuGUI(QWidget *parent)
+			: QMainWindow(parent)
+		{
+			QStringList levelsList=(QStringList()<<"Ekstremalnie latwy"<<"Latwy"<<"Sredni"<<"Trudny"<<"Piekielnie trudny");
+			/*method = 0;
+			level = static_cast<COMPLEXITY_LEVELS>(1);
+			repetitions=0;*/
+			ui.setupUi(this);
+			ui.DifficultyLevelSelector->addItems(levelsList);
+			QObject::connect(ui.okButton, SIGNAL(clicked()),ui.centralWidget,SLOT(okButtonClicked()));
+			QObject::connect(ui.DifficultyLevelSelector, SIGNAL(currentIndexChanged(int)),ui.centralWidget,SLOT(setDifficultyLevel(int)));
+			QObject::connect(ui.RepetitionField, SIGNAL(textChanged(const QString &)),ui.centralWidget, SLOT(setRepetitionCount(const QString &)));
+			QObject::connect(ui.MostNeighboursButton, SIGNAL(clicked()), ui.MethodGroup,SLOT(mostNeighboursSelected()));
+			QObject::connect(ui.RandomButtom, SIGNAL(clicked()),ui.MethodGroup,SLOT(randomSelected()));
+		}
+
+		SudokuGUI::~SudokuGUI()
+		{
+
+		}
--- GUI/sudokugui.h
+++ GUI/sudokugui.h
@@ -3,17 +3,62 @@
 
 #include <QtWidgets/QMainWindow>
 #include "ui_sudokugui.h"
+#include "D:\studia\wmh\projekt\solver\SudokuGenerator.h"
+#include "D:\studia\wmh\projekt\solver\SudokuSolver.h"
+#include <ctime>
+#include <cstdlib>
 
 class SudokuGUI : public QMainWindow
 {
 	Q_OBJECT
 
+		public slots:
+			//Klikniecie przycisku OK
+			void okButtonClicked(){
+				srand(unsigned(time(0)));
+				int* sudokuArray = new int[81]();
+				SudokuGenerator generator;
+				generator.generate(sudokuArray, level);
+				changeCellsValue(sudokuArray);
+				delete[] sudokuArray;
+				//generacja, szukanie rozwiazania,itd
+			}
+			//zaznaczenie sasiadow
+			void mostNeighboursSelected(){
+				method = 0;
+			}
+			//zaznaczenie losowosci
+			void randomSelected(){
+				method = 1;
+			}
+			//wybor z listy poziomow trudnosci
+			void setDifficultyLevel(int lvl){
+				//wydaje mi sie ze indeksowanie od 0
+				level = static_cast<COMPLEXITY_LEVELS>(lvl+1);
+			}
+			void setRepetitionCount(const QString & repCount){
+				repetitions=repCount.toInt();
+			}
 public:
 	SudokuGUI(QWidget *parent = 0);
 	~SudokuGUI();
-
+	int method;
+	COMPLEXITY_LEVELS level;
+	int repetitions;
 private:
 	Ui::SudokuGUIClass ui;
+	void changeCellsValue(int* table){
+		//pewnie bedzie sie przekazywac tablice z wynikiem i tablice wygenerowana i od tego uzalezniac kolor
+
+	}
+
+	void displayTime(string time){
+		ui.TimeField->setText(QString::fromStdString(time));
+	}
+
+	void displayOperations(string operations){
+		ui.OperationsField->setText(QString::fromStdString(operations));
+	}
 };
 
 #endif // SUDOKUGUI_H
