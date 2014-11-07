d04e5a86b751655940731d023b39101726a7cdf3
MJ <bad_boy@o2.pl>
2014-11-07 20:28:14 +0100

    Small fixes

--- GUI/sudokugui.cpp
+++ GUI/sudokugui.cpp
@@ -6,19 +6,19 @@ SudokuGUI::SudokuGUI(QWidget *parent)
     : QMainWindow(parent), m_solveMethod(MOST_NEIGHBOURS), m_level(EXTREMELY_EASY), m_repetitionsCount(0)
 {
     QStringList levelsList=(QStringList()<<"Ekstremalnie \305\202atwy"<<"\305\201atwy"<<"\305\232redni"<<"Trudny"<<"Piekielnie trudny");
-    ui.setupUi(this);
+    m_ui.setupUi(this);
     moveCellsIntoTable();
-    ui.DifficultyLevelSelector->addItems(levelsList);
-    QObject::connect(ui.generateButton, SIGNAL(clicked()),this,SLOT(generateButtonClicked()));
-    QObject::connect(ui.solveButton, SIGNAL(clicked()),this,SLOT(solveButtonClicked()));
-    QObject::connect(ui.DifficultyLevelSelector, SIGNAL(currentIndexChanged(int)),this,SLOT(setDifficultyLevel(int)));
-    QObject::connect(ui.RepetitionField, SIGNAL(textChanged(const QString &)),this, SLOT(setRepetitionCount(const QString &)));
-    QObject::connect(ui.MostNeighboursButton, SIGNAL(clicked()), this,SLOT(mostNeighboursSelected()));
-    QObject::connect(ui.RandomButtom, SIGNAL(clicked()),this,SLOT(randomSelected()));
-    QObject::connect(ui.loadButton, SIGNAL(clicked(bool)),this,SLOT(loadSudokuButtonClicked()));
-    QObject::connect(ui.saveToFileCheckbox, SIGNAL(toggled(bool)),this, SLOT(setSaveFlag(bool)));
-    ui.OperationsField->setText("");
-    ui.TimeField->setText("");
+    m_ui.DifficultyLevelSelector->addItems(levelsList);
+    QObject::connect(m_ui.generateButton, SIGNAL(clicked()),this,SLOT(generateButtonClicked()));
+    QObject::connect(m_ui.solveButton, SIGNAL(clicked()),this,SLOT(solveButtonClicked()));
+    QObject::connect(m_ui.DifficultyLevelSelector, SIGNAL(currentIndexChanged(int)),this,SLOT(setDifficultyLevel(int)));
+    QObject::connect(m_ui.RepetitionField, SIGNAL(textChanged(const QString &)),this, SLOT(setRepetitionCount(const QString &)));
+    QObject::connect(m_ui.MostNeighboursButton, SIGNAL(clicked()), this,SLOT(mostNeighboursSelected()));
+    QObject::connect(m_ui.RandomButtom, SIGNAL(clicked()),this,SLOT(randomSelected()));
+    QObject::connect(m_ui.loadButton, SIGNAL(clicked(bool)),this,SLOT(loadSudokuButtonClicked()));
+    QObject::connect(m_ui.saveToFileCheckbox, SIGNAL(toggled(bool)),this, SLOT(setSaveFlag(bool)));
+    m_ui.OperationsField->setText("");
+    m_ui.TimeField->setText("");
     for (int i = 0; i < 81; ++i)
     {
         m_sudokuGeneratedArray[i] = 0;
@@ -31,7 +31,7 @@ SudokuGUI::SudokuGUI(QWidget *parent)
 void SudokuGUI::displayTime(long long time){
     ostringstream ss;
     ss << time/1000000 << "s " << (time % 1000000)/1000 << "ms " << (time % 1000) << "us ";
-    ui.TimeField->setText(QString::fromStdString(ss.str()));
+    m_ui.TimeField->setText(QString::fromStdString(ss.str()));
 }
 
 void SudokuGUI::changeGridToArray(int* table){
@@ -71,13 +71,13 @@ void SudokuGUI::loadSudokuButtonClicked(){
 }
 
 void SudokuGUI::saveToFile(string stream){
-    myfile << stream << "\n";
+    m_file << stream << "\n";
 }
 
 void SudokuGUI::displayOperations(long long operations) {
     ostringstream ss;
     ss << operations;
-    ui.OperationsField->setText(QString::fromStdString(ss.str()));
+    m_ui.OperationsField->setText(QString::fromStdString(ss.str()));
 }
 
 void SudokuGUI::generateButtonClicked() {
@@ -86,16 +86,16 @@ void SudokuGUI::generateButtonClicked() {
 
 void SudokuGUI::generate() {
     srand(unsigned(time(0)));
-    ui.OperationsField->setText("");
-    ui.TimeField->setText("");
+    m_ui.OperationsField->setText("");
+    m_ui.TimeField->setText("");
     SudokuGenerator generator;
     generator.generate(m_sudokuGeneratedArray, m_level);
     changeCellsValue(m_sudokuGeneratedArray);
 }
 
 void SudokuGUI::solveButtonClicked() {
-    if(saveFlag){
-        myfile.open("result.txt");
+    if(m_saveFlag){
+        m_file.open("result.txt");
     }
     solve();
     unsigned counter = 1;
@@ -105,8 +105,8 @@ void SudokuGUI::solveButtonClicked() {
         generate();
         solve();
     }
-    if(saveFlag){
-        myfile.close();
+    if(m_saveFlag){
+        m_file.close();
     }
 }
 
@@ -118,7 +118,7 @@ void SudokuGUI::solve() {
     displayTime(solver.getSolveTime());
     displayOperations(solver.getSolveComplexity());
     changeCellsValue(m_sudokuSolvedArray, true);
-    if(saveFlag){
+    if(m_saveFlag){
         ostringstream ss;
         ss << "metoda " << m_solveMethod << " poziom trudnosci " << m_level << " liczba operacji " << solver.getSolveComplexity() << 
             " czas " << solver.getSolveTime()/1000000 << "s " << (solver.getSolveTime() % 1000000)/1000 << 
@@ -128,85 +128,85 @@ void SudokuGUI::solve() {
 }
 
 void SudokuGUI::moveCellsIntoTable() {
-    m_cellsArray[0] = ui.cell1;
-    m_cellsArray[1] = ui.cell2;
-    m_cellsArray[2] = ui.cell3;
-    m_cellsArray[3] = ui.cell4;
-    m_cellsArray[4] = ui.cell5;
-    m_cellsArray[5] = ui.cell6;
-    m_cellsArray[6] = ui.cell7;
-    m_cellsArray[7] = ui.cell8;
-    m_cellsArray[8] = ui.cell9;
-    m_cellsArray[9] = ui.cell10;
-    m_cellsArray[10] = ui.cell11;
-    m_cellsArray[11] = ui.cell12;
-    m_cellsArray[12] = ui.cell13;
-    m_cellsArray[13] = ui.cell14;
-    m_cellsArray[14] = ui.cell15;
-    m_cellsArray[15] = ui.cell16;
-    m_cellsArray[16] = ui.cell17;
-    m_cellsArray[17] = ui.cell18;
-    m_cellsArray[18] = ui.cell19;
-    m_cellsArray[19] = ui.cell20;
-    m_cellsArray[20] = ui.cell21;
-    m_cellsArray[21] = ui.cell22;
-    m_cellsArray[22] = ui.cell23;
-    m_cellsArray[23] = ui.cell24;
-    m_cellsArray[24] = ui.cell25;
-    m_cellsArray[25] = ui.cell26;
-    m_cellsArray[26] = ui.cell27;
-    m_cellsArray[27] = ui.cell28;
-    m_cellsArray[28] = ui.cell29;
-    m_cellsArray[29] = ui.cell30;
-    m_cellsArray[30] = ui.cell31;
-    m_cellsArray[31] = ui.cell32;
-    m_cellsArray[32] = ui.cell33;
-    m_cellsArray[33] = ui.cell34;
-    m_cellsArray[34] = ui.cell35;
-    m_cellsArray[35] = ui.cell36;
-    m_cellsArray[36] = ui.cell37;
-    m_cellsArray[37] = ui.cell38;
-    m_cellsArray[38] = ui.cell39;
-    m_cellsArray[39] = ui.cell40;
-    m_cellsArray[40] = ui.cell41;
-    m_cellsArray[41] = ui.cell42;
-    m_cellsArray[42] = ui.cell43;
-    m_cellsArray[43] = ui.cell44;
-    m_cellsArray[44] = ui.cell45;
-    m_cellsArray[45] = ui.cell46;
-    m_cellsArray[46] = ui.cell47;
-    m_cellsArray[47] = ui.cell48;
-    m_cellsArray[48] = ui.cell49;
-    m_cellsArray[49] = ui.cell50;
-    m_cellsArray[50] = ui.cell51;
-    m_cellsArray[51] = ui.cell52;
-    m_cellsArray[52] = ui.cell53;
-    m_cellsArray[53] = ui.cell54;
-    m_cellsArray[54] = ui.cell55;
-    m_cellsArray[55] = ui.cell56;
-    m_cellsArray[56] = ui.cell57;
-    m_cellsArray[57] = ui.cell58;
-    m_cellsArray[58] = ui.cell59;
-    m_cellsArray[59] = ui.cell60;
-    m_cellsArray[60] = ui.cell61;
-    m_cellsArray[61] = ui.cell62;
-    m_cellsArray[62] = ui.cell63;
-    m_cellsArray[63] = ui.cell64;
-    m_cellsArray[64] = ui.cell65;
-    m_cellsArray[65] = ui.cell66;
-    m_cellsArray[66] = ui.cell67;
-    m_cellsArray[67] = ui.cell68;
-    m_cellsArray[68] = ui.cell69;
-    m_cellsArray[69] = ui.cell70;
-    m_cellsArray[70] = ui.cell71;
-    m_cellsArray[71] = ui.cell72;
-    m_cellsArray[72] = ui.cell73;
-    m_cellsArray[73] = ui.cell74;
-    m_cellsArray[74] = ui.cell75;
-    m_cellsArray[75] = ui.cell76;
-    m_cellsArray[76] = ui.cell77;
-    m_cellsArray[77] = ui.cell78;
-    m_cellsArray[78] = ui.cell79;
-    m_cellsArray[79] = ui.cell80;
-    m_cellsArray[80] = ui.cell81;
+    m_cellsArray[0] = m_ui.cell1;
+    m_cellsArray[1] = m_ui.cell2;
+    m_cellsArray[2] = m_ui.cell3;
+    m_cellsArray[3] = m_ui.cell4;
+    m_cellsArray[4] = m_ui.cell5;
+    m_cellsArray[5] = m_ui.cell6;
+    m_cellsArray[6] = m_ui.cell7;
+    m_cellsArray[7] = m_ui.cell8;
+    m_cellsArray[8] = m_ui.cell9;
+    m_cellsArray[9] = m_ui.cell10;
+    m_cellsArray[10] = m_ui.cell11;
+    m_cellsArray[11] = m_ui.cell12;
+    m_cellsArray[12] = m_ui.cell13;
+    m_cellsArray[13] = m_ui.cell14;
+    m_cellsArray[14] = m_ui.cell15;
+    m_cellsArray[15] = m_ui.cell16;
+    m_cellsArray[16] = m_ui.cell17;
+    m_cellsArray[17] = m_ui.cell18;
+    m_cellsArray[18] = m_ui.cell19;
+    m_cellsArray[19] = m_ui.cell20;
+    m_cellsArray[20] = m_ui.cell21;
+    m_cellsArray[21] = m_ui.cell22;
+    m_cellsArray[22] = m_ui.cell23;
+    m_cellsArray[23] = m_ui.cell24;
+    m_cellsArray[24] = m_ui.cell25;
+    m_cellsArray[25] = m_ui.cell26;
+    m_cellsArray[26] = m_ui.cell27;
+    m_cellsArray[27] = m_ui.cell28;
+    m_cellsArray[28] = m_ui.cell29;
+    m_cellsArray[29] = m_ui.cell30;
+    m_cellsArray[30] = m_ui.cell31;
+    m_cellsArray[31] = m_ui.cell32;
+    m_cellsArray[32] = m_ui.cell33;
+    m_cellsArray[33] = m_ui.cell34;
+    m_cellsArray[34] = m_ui.cell35;
+    m_cellsArray[35] = m_ui.cell36;
+    m_cellsArray[36] = m_ui.cell37;
+    m_cellsArray[37] = m_ui.cell38;
+    m_cellsArray[38] = m_ui.cell39;
+    m_cellsArray[39] = m_ui.cell40;
+    m_cellsArray[40] = m_ui.cell41;
+    m_cellsArray[41] = m_ui.cell42;
+    m_cellsArray[42] = m_ui.cell43;
+    m_cellsArray[43] = m_ui.cell44;
+    m_cellsArray[44] = m_ui.cell45;
+    m_cellsArray[45] = m_ui.cell46;
+    m_cellsArray[46] = m_ui.cell47;
+    m_cellsArray[47] = m_ui.cell48;
+    m_cellsArray[48] = m_ui.cell49;
+    m_cellsArray[49] = m_ui.cell50;
+    m_cellsArray[50] = m_ui.cell51;
+    m_cellsArray[51] = m_ui.cell52;
+    m_cellsArray[52] = m_ui.cell53;
+    m_cellsArray[53] = m_ui.cell54;
+    m_cellsArray[54] = m_ui.cell55;
+    m_cellsArray[55] = m_ui.cell56;
+    m_cellsArray[56] = m_ui.cell57;
+    m_cellsArray[57] = m_ui.cell58;
+    m_cellsArray[58] = m_ui.cell59;
+    m_cellsArray[59] = m_ui.cell60;
+    m_cellsArray[60] = m_ui.cell61;
+    m_cellsArray[61] = m_ui.cell62;
+    m_cellsArray[62] = m_ui.cell63;
+    m_cellsArray[63] = m_ui.cell64;
+    m_cellsArray[64] = m_ui.cell65;
+    m_cellsArray[65] = m_ui.cell66;
+    m_cellsArray[66] = m_ui.cell67;
+    m_cellsArray[67] = m_ui.cell68;
+    m_cellsArray[68] = m_ui.cell69;
+    m_cellsArray[69] = m_ui.cell70;
+    m_cellsArray[70] = m_ui.cell71;
+    m_cellsArray[71] = m_ui.cell72;
+    m_cellsArray[72] = m_ui.cell73;
+    m_cellsArray[73] = m_ui.cell74;
+    m_cellsArray[74] = m_ui.cell75;
+    m_cellsArray[75] = m_ui.cell76;
+    m_cellsArray[76] = m_ui.cell77;
+    m_cellsArray[77] = m_ui.cell78;
+    m_cellsArray[78] = m_ui.cell79;
+    m_cellsArray[79] = m_ui.cell80;
+    m_cellsArray[80] = m_ui.cell81;
 }
--- GUI/sudokugui.h
+++ GUI/sudokugui.h
@@ -23,7 +23,7 @@ class SudokuGUI : public QMainWindow
         void randomSelected() { m_solveMethod = RANDOM; }
         void setDifficultyLevel(int lvl) { m_level = static_cast<COMPLEXITY_LEVELS>(lvl+1); }
         void setRepetitionCount(const QString & repCount) { m_repetitionsCount = repCount.toInt(); }
-        void setSaveFlag(bool flag) { saveFlag = flag;}
+        void setSaveFlag(bool flag) { m_saveFlag = flag; }
 
     public:
         SudokuGUI(QWidget *parent = 0);
@@ -38,14 +38,14 @@ class SudokuGUI : public QMainWindow
         void solve();
         void saveToFile(string stream);
 
-        ofstream myfile;
+        ofstream m_file;
         unsigned m_repetitionsCount;        
         int m_sudokuGeneratedArray[81];
         int m_sudokuSolvedArray[81];
-        bool saveFlag;
+        bool m_saveFlag;
         NEXT_POINT_SEARCHING_SCENARIO m_solveMethod;
         COMPLEXITY_LEVELS m_level;
-        Ui::SudokuGUIClass ui;
+        Ui::SudokuGUIClass m_ui;
         QLineEdit* m_cellsArray[81];
 };
 
